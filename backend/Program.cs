af57b1d145136401b1c0d3807aeaacfafc389dbf
Julius Walton <juliuswalton7901@gmail.com>
2026-01-18 03:21:43 -0500

    switch backend to dotnet

---
+++ backend/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+app.MapGet("/api/health", () => new { status = "ok" });
+
+app.Run();
--- backend/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/ShiosOS/cauldron
-
-go 1.25.6
-
-require (
-	github.com/go-chi/chi/v5 v5.2.4 // indirect
-	github.com/go-chi/cors v1.2.2 // indirect
-)
